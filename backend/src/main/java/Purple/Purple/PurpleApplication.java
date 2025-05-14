3525f7ecb4050e9080c8dc242c63560b3add3605
hyeyeon Cho <chohyeyeon1125@gmail.com>
2025-05-14 15:37:25 +0900

    chore: 프론트엔드 디렉토리 생성

--- backend/src/main/java/Purple/Purple/PurpleApplication.java
+++ backend/src/main/java/Purple/Purple/PurpleApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PurpleApplication {
+public class PurpleApplication {	// 프론트엔드 디렉토리 만들기
 
 	public static void main(String[] args) {
 		SpringApplication.run(PurpleApplication.class, args);
