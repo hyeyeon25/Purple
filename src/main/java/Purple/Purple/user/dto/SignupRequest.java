ddf9d55451921d115b131ddc8279066986430723
2unhy2 <144293456+2unhy2@users.noreply.github.com>
2025-10-02 15:52:02 +0900

    회원가입 로직 리팩토링 및 필드 추가함

--- src/main/java/Purple/Purple/user/dto/SignupRequest.java
+++ src/main/java/Purple/Purple/user/dto/SignupRequest.java
@@ -1,9 +1,12 @@
 package Purple.Purple.user.dto;
 
+import Purple.Purple.enums.Gender;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDate;
 
 @Getter
 @Setter
@@ -16,9 +19,12 @@ public class SignupRequest {
     @Schema(description = "사용자 비밀번호", example = "password123", required = true)
     private String password;
 
-    @Schema(description = "사용자 이름", example = "김퍼플", required = true)
-    private String userName;
-
     @Schema(description = "닉네임", example = "보라돌이", required = false)
     private String nickname;
+
+    @Schema(description = "사용자 성별", example = "MALE")
+    private Gender gender;
+
+    @Schema(description = "사용자 생일", example = "yyyy-MM-dd")
+    private LocalDate birthDate;
 }
--- src/main/java/Purple/Purple/user/dto/UserResponse.java
+++ src/main/java/Purple/Purple/user/dto/UserResponse.java
@@ -1,8 +1,11 @@
 package Purple.Purple.user.dto;
 
+import Purple.Purple.enums.Gender;
 import Purple.Purple.user.entity.UserPersonalInfo;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
+
+import java.time.LocalDate;
 
 @Getter
 @Schema(description = "사용자 정보 응답 DTO")
@@ -13,17 +16,23 @@ public class UserResponse {
     @Schema(description = "사용자 이메일", example = "user@example.com")
     private String email;
 
-    @Schema(description = "사용자 이름", example = "김퍼플")
-    private String userName;
-
     @Schema(description = "닉네임", example = "보라돌이")
     private String nickname;
 
-    public UserResponse(Long userId, String email, String userName, String nickname) {
+    @Schema(description = "사용자 성별", example = "MALE")
+    private Gender gender;
+
+    @Schema(description = "사용자 생일", example = "yyyy-MM-dd")
+    private LocalDate birthDate;
+
+    public UserResponse(Long userId, String email, String nickname, Gender gender, LocalDate birthDate) {
         this.userId = userId;
         this.email = email;
-        this.userName = userName;
         this.nickname = nickname;
+        this.gender = gender;
+        this.birthDate = birthDate;
     }
+
+
 
 }
