bda0c9188509f4b0ee3d6c53ecb895de564d1e9a
moonsohe <thgml89567@naver.com>
2025-10-02 20:11:43 +0900

    feat: DB에 데이터 place 저장할 때 score 채우기

--- src/main/java/Purple/Purple/place/service/PlaceService.java
+++ src/main/java/Purple/Purple/place/service/PlaceService.java
@@ -1,13 +1,6 @@
 package Purple.Purple.place.service;
 
 import Purple.Purple.place.dto.PlaceResponseDto;
-import Purple.Purple.place.entity.PlaceEntity;
-
-import java.util.List;
-
-// Service 인터페이스
-// - 장소 데이터 처리와 관련된 핵심 비즈니스 로직의 명세를 정의합니다.
-//- Controller는 이 인터페이스에 의존합니다.
 
 public interface PlaceService {
 
