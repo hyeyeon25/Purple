2c17527dd88e767998835b0e6c69389913cd1030
moonsohe <thgml89567@naver.com>
2025-11-03 15:02:38 +0900

    commit

--- src/main/java/Purple/Purple/PurpleApplication.java
+++ src/main/java/Purple/Purple/PurpleApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PurpleApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PurpleApplication.class, args);
 	}
