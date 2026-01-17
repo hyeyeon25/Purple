b1175cd8aba8ce845a02659c4c809f5810b51e76
moonsohe <thgml89567@naver.com>
2026-01-18 00:54:50 +0900

    refact: kakao 파일 리팩토링

--- src/main/java/Purple/Purple/kakao/dto/KakaoMeta.java
+++ src/main/java/Purple/Purple/kakao/dto/KakaoMeta.java
@@ -4,11 +4,9 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 /**
- * Kakao Local Search API의 meta 부분
- * - 검색 결과의 전체 개수(totalCount), 페이지 가능 개수(pageableCount),
- *   마지막 페이지 여부(isEnd)를 알려줌
- *
- * 페이지네이션 처리할 때 꼭 필요한 정보
+ * Kakao Local Search API에서
+ * - 검색 결과의 전체 개수(total_count), 페이지 가능 개수(pageable_count),
+ *   마지막 페이지 여부(is_end)를 알려줌
  */
 @Getter @Setter
 @NoArgsConstructor @AllArgsConstructor
